4b72ab432ae0b3961199953a3a25acaa113694e1
Rizqiansyah Ramadhan <thenoblerizz@gmail.com>
2023-12-14 20:28:04 +0700

    change field model

--- server/src/models/Post.ts
+++ server/src/models/Post.ts
@@ -6,14 +6,9 @@ interface IPost {
   description: string;
   tags: mongoose.Schema.Types.ObjectId[];
   images: string[];
-  upvotes: {
-    userId: mongoose.Schema.Types.ObjectId[];
-    count: number;
-  };
-  downvotes: {
-    userId: mongoose.Schema.Types.ObjectId[];
-    count: number;
-  };
+  upvotes: mongoose.Schema.Types.ObjectId[];
+  downvotes: mongoose.Schema.Types.ObjectId[];
+  voteCount: number;
   commentsCount: number;
 }
 
@@ -26,14 +21,9 @@ const PostSchema = new mongoose.Schema<PostDocument>(
     tags: { type: [mongoose.SchemaTypes.ObjectId], ref: "Tag" },
     images: [String],
     description: String,
-    upvotes: {
-      userId: { type: [mongoose.SchemaTypes.ObjectId], ref: "User", default: [] },
-      count: { type: Number, default: 0 },
-    },
-    downvotes: {
-      userId: { type: [mongoose.SchemaTypes.ObjectId], ref: "User", default: [] },
-      count: { type: Number, default: 0 },
-    },
+    upvotes: { type: [mongoose.SchemaTypes.ObjectId], ref: "User", default: [] },
+    downvotes: { type: [mongoose.SchemaTypes.ObjectId], ref: "User", default: [] },
+    voteCount: { type: Number, default: 0 },
     commentsCount: { type: Number, default: 0 },
   },
   { timestamps: true }
